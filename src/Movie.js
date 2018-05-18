a41f7d76d918418123d6b8fcbf2a3b9e98f04d76
leegangsan <nouvin@certon.co.kr>
2018-05-18 15:10:09 +0900

    Data flow with Props

--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,13 @@
 import React, {Component } from 'react';
 import './Movie.css';
 
+
 class Movie extends Component {
   render(){
     return(
       <div className="">
-        <MoviePoster />
-        <h1>hello this is a movie</h1>
+        <MoviePoster poster={this.props.poster}/>
+        <h1>{this.props.title}</h1>
       </div>
     )
   }
@@ -15,7 +16,7 @@ class Movie extends Component {
 class MoviePoster extends Component{
   render(){
     return(
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlJQt4RT47Io_tT0ukKfub1f8SxOSX-rH20Sa5f9dQ3trUp4U2" alt=""/>
+      <img src={this.props.poster} alt=""/>
     )
   }
 }
