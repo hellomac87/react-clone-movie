285dfc4de9caffef52b0cdb535819c6f1065588d
leegangsan <nouvin@certon.co.kr>
2018-05-18 17:59:19 +0900

    Loading states

--- src/App.js
+++ src/App.js
@@ -7,48 +7,49 @@ class App extends Component {
   // Update : componentWillReceiveProps() => shouldComponentUpdate() => componentWillUpdate() => render() => componentDidUpdate()
 
   state = {
-    greeting : "hello!",
-    movies : [
-      {
-        title:"Matrix",
-        poster:"https://ia.media-imdb.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
-      },
-      {
-        title:"Full Metal Jacket",
-        poster:"https://resizing.flixster.com/xqFW8SryVoj3g4hezXKsNJr9P9o=/206x305/v1.bTsxMTE2ODAyOTtqOzE3NzY5OzEyMDA7ODAwOzEyMDA",
-      },
-      {
-        title:"Oldboy",
-        poster:"https://images-na.ssl-images-amazon.com/images/I/91ONQ8FNHJL._SY445_.jpg",
-      },
-      {
-        title:"Star Wars",
-        poster:"https://lumiere-a.akamaihd.net/v1/images/the-last-jedi-theatrical-poster-film-page_bca06283.jpeg?region=0%2C0%2C480%2C711"
-      },
-    ]
+    
   };
 
   componentDidMount(){
     setTimeout(() => {
       this.setState({
         movies : [
-          //...this.state.movies, // 이전 내용을 그대로 두고, 내용을 추가하라
+          {
+            title:"Matrix",
+            poster:"https://ia.media-imdb.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
+          },
+          {
+            title:"Full Metal Jacket",
+            poster:"https://resizing.flixster.com/xqFW8SryVoj3g4hezXKsNJr9P9o=/206x305/v1.bTsxMTE2ODAyOTtqOzE3NzY5OzEyMDA7ODAwOzEyMDA",
+          },
+          {
+            title:"Oldboy",
+            poster:"https://images-na.ssl-images-amazon.com/images/I/91ONQ8FNHJL._SY445_.jpg",
+          },
+          {
+            title:"Star Wars",
+            poster:"https://lumiere-a.akamaihd.net/v1/images/the-last-jedi-theatrical-poster-film-page_bca06283.jpeg?region=0%2C0%2C480%2C711"
+          },
           {
             title: "Dead Pool",
             poster:"https://assets.voxcinemas.com/posters/P_HO00005265.jpg"
           },
-          ...this.state.movies //뒤에 붙이면 새로운 내용위 먼저 추가됨
         ]
       });
     },5000);
   }
 
+  _renderMovies = () => {
+    const movies = this.state.movies.map( (movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    });
+    return movies;
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.movies.map( (movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index}/>
-        })}
+        {this.state.movies ? this._renderMovies() : "Loading..."}
       </div>
     );
   }
