d889af4fbf7193f941bdcd8371b4fb9de4bbb4ff
leegangsan <nouvin@certon.co.kr>
2018-05-18 15:36:57 +0900

    Lists with .maps

--- src/App.js
+++ src/App.js
@@ -2,40 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movieTitles = [
-  "Matrix",
-  "Full Metal Jacket",
-  "Oldboy",
-  "Star Wars"
+const movies = [
+  {
+    title:"Matrix",
+    poster:"https://ia.media-imdb.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
+  },
+  {
+    title:"Full Metal Jacket",
+    poster:"https://resizing.flixster.com/xqFW8SryVoj3g4hezXKsNJr9P9o=/206x305/v1.bTsxMTE2ODAyOTtqOzE3NzY5OzEyMDA7ODAwOzEyMDA",
+  },
+  {
+    title:"Oldboy",
+    poster:"https://images-na.ssl-images-amazon.com/images/I/91ONQ8FNHJL._SY445_.jpg",
+  },
+  {
+    title:"Star Wars",
+    poster:"https://lumiere-a.akamaihd.net/v1/images/the-last-jedi-theatrical-poster-film-page_bca06283.jpeg?region=0%2C0%2C480%2C711"
+  },
 ]
 
-const movieImages = [
-  "https://ia.media-imdb.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
-  "https://resizing.flixster.com/xqFW8SryVoj3g4hezXKsNJr9P9o=/206x305/v1.bTsxMTE2ODAyOTtqOzE3NzY5OzEyMDA7ODAwOzEyMDA",
-  "https://images-na.ssl-images-amazon.com/images/I/91ONQ8FNHJL._SY445_.jpg",
-  "https://lumiere-a.akamaihd.net/v1/images/the-last-jedi-theatrical-poster-film-page_bca06283.jpeg?region=0%2C0%2C480%2C711"
-]
- 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie 
-        title={movieTitles[0]}
-        poster={movieImages[0]}
-        />
-        <Movie 
-        title={movieTitles[1]}
-        poster={movieImages[1]}
-        />
-        <Movie 
-        title={movieTitles[2]}
-        poster={movieImages[2]}
-        />
-        <Movie 
-        title={movieTitles[3]}
-        poster={movieImages[3]}
-        />
+        {movies.map(movie => {
+          return <Movie title={movie.title} poster={movie.poster}/>
+        })}
       </div>
     );
   }
