10aab434581542e70caa0314f95e483b8234cce5
leegangsan <nouvin@certon.co.kr>
2018-05-18 17:48:17 +0900

    Practicing this setState

--- src/App.js
+++ src/App.js
@@ -2,37 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title:"Matrix",
-    poster:"https://ia.media-imdb.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
-  },
-  {
-    title:"Full Metal Jacket",
-    poster:"https://resizing.flixster.com/xqFW8SryVoj3g4hezXKsNJr9P9o=/206x305/v1.bTsxMTE2ODAyOTtqOzE3NzY5OzEyMDA7ODAwOzEyMDA",
-  },
-  {
-    title:"Oldboy",
-    poster:"https://images-na.ssl-images-amazon.com/images/I/91ONQ8FNHJL._SY445_.jpg",
-  },
-  {
-    title:"Star Wars",
-    poster:"https://lumiere-a.akamaihd.net/v1/images/the-last-jedi-theatrical-poster-film-page_bca06283.jpeg?region=0%2C0%2C480%2C711"
-  },
-]
-
 class App extends Component {
   // Render : componentWillMount() => render() => componentDidMount()
   // Update : componentWillReceiveProps() => shouldComponentUpdate() => componentWillUpdate() => render() => componentDidUpdate()
 
   state = {
-    greeting : "hello!"
+    greeting : "hello!",
+    movies : [
+      {
+        title:"Matrix",
+        poster:"https://ia.media-imdb.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
+      },
+      {
+        title:"Full Metal Jacket",
+        poster:"https://resizing.flixster.com/xqFW8SryVoj3g4hezXKsNJr9P9o=/206x305/v1.bTsxMTE2ODAyOTtqOzE3NzY5OzEyMDA7ODAwOzEyMDA",
+      },
+      {
+        title:"Oldboy",
+        poster:"https://images-na.ssl-images-amazon.com/images/I/91ONQ8FNHJL._SY445_.jpg",
+      },
+      {
+        title:"Star Wars",
+        poster:"https://lumiere-a.akamaihd.net/v1/images/the-last-jedi-theatrical-poster-film-page_bca06283.jpeg?region=0%2C0%2C480%2C711"
+      },
+    ]
   };
 
   componentDidMount(){
     setTimeout(() => {
       this.setState({
-        greeting : "Hello Again"
+        movies : [
+          //...this.state.movies, // 이전 내용을 그대로 두고, 내용을 추가하라
+          {
+            title: "Dead Pool",
+            poster:"https://assets.voxcinemas.com/posters/P_HO00005265.jpg"
+          },
+          ...this.state.movies //뒤에 붙이면 새로운 내용위 먼저 추가됨
+        ]
       });
     },5000);
   }
@@ -40,8 +46,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {movies.map( (movie, index) => {
+        {this.state.movies.map( (movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index}/>
         })}
       </div>
