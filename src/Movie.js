81e6156358d437e1ac82f7d9778e55a373dfebff
leegangsan <nouvin@certon.co.kr>
2018-05-21 14:52:31 +0900

    Updating Movie

--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
+import { spawn } from 'child_process';
 
-function Movie({title,poster}){
+function Movie({title,poster,genres,synopsis}){
   return (
-    <div className="">
-      <MoviePoster poster={poster}/>
-      <h1>{title}</h1>
+    <div className="Movie">
+      <div className="Movie__Columns">
+        <MoviePoster poster={poster} alt={title}/>
+      </div>
+      <div className="Movie__Columns">
+        <h1>{title}</h1>
+        <div className="Movie_Genres">
+          {genres.map((genre, index) => <MovieGenre genre={genre} key={index}/>)}
+        </div>
+        <p className="Movie__Synopsis">
+          {synopsis}
+        </p>
+      </div>
     </div>
   )
 }
@@ -14,15 +25,29 @@ function Movie({title,poster}){
 Movie.propTypes = {
   title : PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres : PropTypes.array.isRequired,
+  synopsis: PropTypes.string.isRequired,
 }
 
-function MoviePoster({poster}){
+function MoviePoster({poster,alt}){
   return(
-    <img src={poster} alt=""/>
+    <img src={poster} className="Movie__poster" alt={alt} title={alt}/>
   )
 }
 
 MoviePoster.propTypes = {
   poster: PropTypes.string.isRequired,
+  alt:PropTypes.string.isRequired,
 }
+
+function MovieGenre({genre}){
+  return(
+    <span className="Movie__Genre">{genre} </span>
+  )
+}
+
+MovieGenre.propTypes = {
+  poster: PropTypes.array.isRequired,
+}
+
 export default Movie;
